12608bd4eda876930bf19001491874c36e04a684
ahmed hussien <33827384+eng-ahmedhussien@users.noreply.github.com>
2026-01-20 12:30:20 +0200

    Initial Commit

---
+++ CleanArchitectureExampleVersions/CleanArchitectureExampleVersionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureExampleVersionsApp.swift
+//  CleanArchitectureExampleVersions
+//
+//  Created by Ahmed Hussien on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureExampleVersionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
